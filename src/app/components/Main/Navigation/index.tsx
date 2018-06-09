7cf2ca19fa8e8192ffb62f27b6a48752947a218a
Saurer <redmmond@gmail.com>
2018-06-09 21:37:59 +0900

    Fixed navigation

--- src/app/components/Main/Navigation/index.tsx
+++ src/app/components/Main/Navigation/index.tsx
@@ -27,7 +27,7 @@ import StackGroup from 'components/Animation/StackGroup';
 import themed from 'components/Theme/themed';
 import Protypo from 'containers/Widgets/Protypo';
 import ResizeHandle from 'containers/Main/Navigation/ResizeHandle';
-import { TMenu } from 'genesis/content';
+import { TMenu } from 'apla/content';
 import ScrollArea from 'react-scrollbar';
 
 const StyledNavigation = themed.aside`
