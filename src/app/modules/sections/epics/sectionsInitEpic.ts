35c78b74d5295a1cd075579d92d35c91ca119214
Saurer <redmmond@gmail.com>
2018-06-15 01:27:32 +0900

    Fix conflicts

--- src/app/modules/sections/epics/sectionsInitEpic.ts
+++ src/app/modules/sections/epics/sectionsInitEpic.ts
@@ -1,17 +1,17 @@
 // MIT License
-//
-// Copyright (c) 2016-2018 GenesisKernel
-//
+// 
+// Copyright (c) 2016-2018 AplaProject
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,7 +24,7 @@ import { Epic } from 'modules';
 import { Observable } from 'rxjs/Observable';
 import { setDefaultPage, sectionsInit } from 'modules/sections/actions';
 
-const sectionsInitDoneEpic: Epic = (action$, store, { api }) => action$.ofAction(sectionsInit.done)
+const sectionsInitEpic: Epic = (action$, store, { api }) => action$.ofAction(sectionsInit)
     .flatMap(action => {
         const state = store.getState();
         const client = api(state.auth.session);
@@ -37,13 +37,13 @@ const sectionsInitDoneEpic: Epic = (action$, store, { api }) => action$.ofAction
                 .then(row => row.value.default_page)
         ).flatMap(payload =>
             Observable.of(
-                setDefaultPage(payload)
+                setDefaultPage(payload ? payload : 'default_page')
             )
         );
     }).catch(e =>
         Observable.of(
-            setDefaultPage(null)
+            setDefaultPage('default_page')
         )
     );
 
-export default sectionsInitDoneEpic;
+export default sectionsInitEpic;
