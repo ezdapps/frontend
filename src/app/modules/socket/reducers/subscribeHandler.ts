3d057f803b77700581ba73be3ce5e76b0e07b26e
Frank Horrigan <redmmond@gmail.com>
2019-07-22 10:36:08 +1000

    Fix notification count bootstrap

--- src/app/modules/socket/reducers/subscribeHandler.ts
+++ src/app/modules/socket/reducers/subscribeHandler.ts
@@ -6,18 +6,30 @@
 import { State } from '../reducer';
 import { subscribe } from '../actions';
 import { Reducer } from 'modules';
+import { IAccount } from 'apla/api';
+import { INotificationsMessage } from 'apla/socket';
+
+const flattenNotifications = (id: string, info: IAccount) => {
+    const stack: INotificationsMessage[] = [];
+    info.access.forEach(ecosystem => {
+        ecosystem.notifications.forEach(notification => {
+            stack.push({
+                id,
+                ecosystem: ecosystem.ecosystem,
+                role: notification.role_id,
+                count: Number(notification.count)
+            });
+        });
+    });
+
+    return stack;
+};
 
 const subscribeHandler: Reducer<typeof subscribe.started, State> = (state, payload) => ({
     ...state,
     notifications: [
         ...state.notifications.filter(l => l.id !== payload.id),
-        ...payload.access.map(ecosystem => Array.prototype.concat.apply([], ecosystem.notifications.map(
-            notification => ({
-                ecosystem: ecosystem.ecosystem,
-                role: notification.role_id,
-                count: notification.count
-            }))
-        ))
+        ...flattenNotifications(payload.id, payload)
     ]
 });
 
