77087ff975256794109171f5b307bb0f79b1963f
Frank Horrigan <redmmond@gmail.com>
2019-07-26 16:41:15 +1000

    Remove menu handler from static pages

--- src/app/lib/staticPages.tsx
+++ src/app/lib/staticPages.tsx
@@ -8,14 +8,13 @@ import Backup from 'containers/Main/Backup';
 import Editor from 'containers/Main/Editor';
 
 export interface IStaticPage {
-    menu: string;
     section: string;
     render: (props?: { [key: string]: any }) => React.ReactNode;
 }
 
 const STATIC_PAGES: { [page: string]: IStaticPage } = {
-    'backup': { section: null, menu: null, render: () => <Backup /> },
-    'editor': { section: 'editor', menu: null, render: (props: { open?: string, create?: string, name?: string, vde?: string }) => <Editor {...props} /> }
+    'backup': { section: null, render: () => <Backup /> },
+    'editor': { section: 'editor', render: (props: { open?: string, create?: string, name?: string, vde?: string }) => <Editor {...props} /> }
 };
 
 export {
