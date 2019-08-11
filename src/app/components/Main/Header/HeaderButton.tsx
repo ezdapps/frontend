8dfb50f17f1418adb932b30794f44b33a3f80446
Frank Horrigan <redmmond@gmail.com>
2019-08-11 11:55:07 +1000

    Fix headerbutton not showing badge

--- src/app/components/Main/Header/HeaderButton.tsx
+++ src/app/components/Main/Header/HeaderButton.tsx
@@ -41,6 +41,20 @@ const StyledHeaderButton = themed(DropdownButton)`
     &._active {
         color: ${props => props.theme.menubarForegroundActive};
     }
+
+    .dropdown__badge {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        background: #d46565;
+        display: block;
+        width: 16px;
+        height: 16px;
+        padding: 0;
+        line-height: 16px;
+        font-size: 12px;
+        font-weight: bold;
+    }
 `;
 
 const HeaderButton: React.SFC<Props> = props => (
@@ -55,6 +69,11 @@ const HeaderButton: React.SFC<Props> = props => (
         menuWidth={props.menuWidth}
     >
         {props.children}
+        {props.badge ? (
+            <em className="dropdown__badge">
+                {Math.min(props.badge, 99)}
+            </em>
+        ) : null}
     </StyledHeaderButton>
 );
 
