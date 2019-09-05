72cbd3c8d66588ead0d91655c8e43d63ee0d6c46
Frank Horrigan <redmmond@gmail.com>
2019-09-06 03:30:04 +1000

    Update share account modal look & feel

--- src/app/components/Modal/Auth/CopyWalletModal.tsx
+++ src/app/components/Modal/Auth/CopyWalletModal.tsx
@@ -4,19 +4,22 @@
  *--------------------------------------------------------------------------------------------*/
 
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
 import { IWallet } from 'apla/auth';
 import QRCode from 'qrcode.react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 import Modal from '../';
+import ModalWindow from 'containers/Modal/ModalWindow';
+import Button from 'components/Button/Button';
 
 export interface ICopyWalletModalParams {
     wallet: IWallet;
 }
 
 class CopyWalletModal extends Modal<ICopyWalletModalParams, void> {
+    public static className = ' ';
+
     formatKey = (key: string) => {
         return key.match(/.{1,2}/g).join(' ');
     }
@@ -27,45 +30,63 @@ class CopyWalletModal extends Modal<ICopyWalletModalParams, void> {
 
     render() {
         return (
-            <div>
-                <Modal.Header>
-                    <FormattedMessage id="auth.wallet.share.long" defaultMessage="Share account" />
-                </Modal.Header>
-                <Modal.Body>
-                    <table className="table table-striped table-bordered table-hover preline mb0" style={{ maxWidth: 500 }}>
-                        <tbody>
-                            <tr>
-                                <td style={{ minWidth: 100 }}>
-                                    <FormattedMessage id="general.key.public" defaultMessage="Public key" />
-                                </td>
-                                <td>{this.formatKey(this.props.params.wallet.publicKey)}</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <FormattedMessage id="auth.qrcode" defaultMessage="QR-Code" />
-                                </td>
-                                <td>
-                                    <div className="text-center">
-                                        <QRCode value={this.getCopyPayload()} />
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className="text-center">
-                        <CopyToClipboard text={this.getCopyPayload()}>
-                            <Button bsStyle="link">
-                                <FormattedMessage id="general.clipboard.copy" defaultMessage="Copy to clipboard" />
-                            </Button>
-                        </CopyToClipboard>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer className="text-right">
-                    <Button type="button" bsStyle="primary" onClick={this.props.onCancel}>
+            <ModalWindow
+                title={
+                    <FormattedMessage
+                        id="auth.wallet.share.long"
+                        defaultMessage="Share account"
+                    />
+                }
+                controls={(
+                    <Button type="primary" onClick={this.props.onCancel}>
                         <FormattedMessage id="close" defaultMessage="Close" />
                     </Button>
-                </Modal.Footer>
-            </div>
+                )}
+            >
+                <table
+                    className="mt table table-striped table-bordered table-hover preline mb0"
+                    style={{ maxWidth: 500 }}
+                >
+                    <tbody>
+                        <tr>
+                            <td style={{ minWidth: 100 }}>
+                                <FormattedMessage
+                                    id="general.key.public"
+                                    defaultMessage="Public key"
+                                />
+                            </td>
+                            <td>
+                                {this.formatKey(
+                                    this.props.params.wallet.publicKey
+                                )}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <FormattedMessage
+                                    id="auth.qrcode"
+                                    defaultMessage="QR-Code"
+                                />
+                            </td>
+                            <td>
+                                <div className="text-center">
+                                    <QRCode value={this.getCopyPayload()} />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="text-center">
+                    <CopyToClipboard text={this.getCopyPayload()}>
+                        <Button type="link">
+                            <FormattedMessage
+                                id="general.clipboard.copy"
+                                defaultMessage="Copy to clipboard"
+                            />
+                        </Button>
+                    </CopyToClipboard>
+                </div>
+            </ModalWindow>
         );
     }
 }
