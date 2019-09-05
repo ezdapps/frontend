2a8ee80343c475e5e81800aa067cac1403025202
Frank Horrigan <redmmond@gmail.com>
2019-09-06 09:45:55 +1000

    Update password change success modal design

--- src/app/components/Modal/Auth/AuthPasswordChangedModal.tsx
+++ src/app/components/Modal/Auth/AuthPasswordChangedModal.tsx
@@ -3,28 +3,34 @@
  *  See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import ModalWindow from 'containers/Modal/ModalWindow';
 
 import Modal from '../';
+import Button from 'components/Button/Button';
 
 class AuthPasswordChangedModal extends Modal<{}, void> {
     render() {
         return (
-            <div>
-                <Modal.Header>
-                    <FormattedMessage id="alert.info" defaultMessage="Information" />
-                </Modal.Header>
-                <Modal.Body>
-                    <div><FormattedMessage id="auth.password.changed" defaultMessage="Password changed. Please login with new password" /></div>
-                </Modal.Body>
-                <Modal.Footer className="text-right">
-                    <Button type="button" bsStyle="primary" onClick={this.props.onCancel.bind(this)}>
+            <ModalWindow
+                title={
+                    <FormattedMessage
+                        id="alert.info"
+                        defaultMessage="Information"
+                    />
+                }
+                controls={(
+                    <Button onClick={this.props.onCancel}>
                         <FormattedMessage id="close" defaultMessage="Close" />
                     </Button>
-                </Modal.Footer>
-            </div>
+                )}
+            >
+                <FormattedMessage
+                    id="auth.password.changed"
+                    defaultMessage="Password changed. Please login with new password"
+                />
+            </ModalWindow>
         );
     }
 }
