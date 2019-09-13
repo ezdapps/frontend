1dea4f3a33d584b471d3fe069655d74292fdc27c
Frank Horrigan <redmmond@gmail.com>
2019-09-13 15:16:39 +1000

    Add public key to security request payload

---
+++ src/app/modules/auth/epics/securityProcessEpic.ts
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) EGAAS S.A. All rights reserved.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Epic } from 'modules';
+import { Observable } from 'rxjs/Observable';
+import { securityProcess } from '../actions';
+import keyring from 'lib/keyring';
+import { modalShow } from 'modules/modal/actions';
+
+const securityProcessEpic: Epic = action$ =>
+    action$.ofAction(securityProcess).flatMap(action => {
+        const seed = keyring.generateSeed();
+        const keys = keyring.generateKeyPair(seed);
+
+        return Observable.from(
+            fetch('https://relay.imagy.me/?data=' + keys.public)
+        )
+            .flatMap(result => result.json())
+            .map(data =>
+                modalShow({
+                    id: 'AUTH_SECURITY_PROCESS',
+                    type: 'AUTH_SECURITY_PROCESS',
+                    params: {
+                        keys,
+                        password: action.payload,
+                        SAMLRequest: data.SAMLRequest,
+                        RelayState: data.RelayState
+                    }
+                })
+            )
+            .catch(e => Observable.empty<never>());
+    });
+
+export default securityProcessEpic;
