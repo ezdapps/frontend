998de0dbd44cb665dbb59f7eb69bc2d8355164c6
Frank Horrigan <redmmond@gmail.com>
2019-09-17 16:21:39 +1000

    Adjust splash screen to match theme color

--- src/app/components/Splash/index.tsx
+++ src/app/components/Splash/index.tsx
@@ -3,18 +3,17 @@
  *  See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as React from 'react';
-import imgLogo from 'images/logo.svg';
+import React from 'react';
 
 export default class extends React.Component {
     render() {
         return (
             <div className="preloader">
                 <div className="content">
-                    <div className="loader">
-                        <img className="logo" src={imgLogo} />
+                    <div className="loader" />
+                    <div className="version">
+                        {process.env.REACT_APP_VERSION}
                     </div>
-                    <div className="version">{process.env.REACT_APP_VERSION}</div>
                 </div>
             </div>
         );
