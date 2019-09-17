62ff8e6afb45feba6a0950c4f5550a35b01a047d
Frank Horrigan <redmmond@gmail.com>
2019-09-17 20:34:23 +1000

    Show only 1 breadcrumb on mobile

--- src/app/components/Main/Navigator/Sections/Breadcrumbs.tsx
+++ src/app/components/Main/Navigator/Sections/Breadcrumbs.tsx
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import React from 'react';
+import classNames from 'classnames';
 import { IBreadcrumb } from 'apla/content';
 import { FormattedMessage } from 'react-intl';
 
@@ -11,6 +12,7 @@ import themed from 'components/Theme/themed';
 import Toolbar from 'components/Main/Toolbar';
 import Breadcrumb from './Breadcrumb';
 import ToolButton from 'components/Main/Toolbar/ToolButton';
+import media from 'components/Theme/media';
 
 interface Props {
     values: IBreadcrumb[];
@@ -47,13 +49,29 @@ const StyledBreadcrumbs = themed.ul`
             margin-right: 8px;
         }
     }
+
+
+    @media(${media.md}) {
+        > li.breadcrumbs__breadcrumb_normal {
+            display: none;
+        }
+    }
 `;
 
-const Breadcrumbs: React.SFC<Props> = (props) => (
+const Breadcrumbs: React.SFC<Props> = props => (
     <Toolbar>
         <StyledBreadcrumbs>
             {props.values.map((breadcrumb, i) => (
-                <li key={i}>
+                <li
+                    key={i}
+                    className={classNames({
+                        breadcrumbs__breadcrumb_home: 0 === i,
+                        breadcrumbs__breadcrumb_normal:
+                            props.values.length - 1 > i && 0 !== i,
+                        breadcrumbs__breadcrumb_current:
+                            props.values.length - 1 === i
+                    })}
+                >
                     <Breadcrumb
                         home={i === 0}
                         active={i !== props.values.length - 1}
@@ -66,19 +84,21 @@ const Breadcrumbs: React.SFC<Props> = (props) => (
                 </li>
             ))}
             {1 === props.values.length && (
-                <li>
-                    <Breadcrumb
-                        section=""
-                        page=""
-                        params={{}}
-                    >
-                        <FormattedMessage id="navigation.default_page" defaultMessage="Home page" />
+                <li className="breadcrumbs__breadcrumb_current">
+                    <Breadcrumb section="" page="" params={{}}>
+                        <FormattedMessage
+                            id="navigation.default_page"
+                            defaultMessage="Home page"
+                        />
                     </Breadcrumb>
                 </li>
             )}
         </StyledBreadcrumbs>
         <ToolButton icon="icon-refresh" onClick={props.onRefresh}>
-            <FormattedMessage id="navigation.refresh" defaultMessage="Refresh" />
+            <FormattedMessage
+                id="navigation.refresh"
+                defaultMessage="Refresh"
+            />
         </ToolButton>
     </Toolbar>
 );
