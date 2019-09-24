4a5396383de5eaccb6711e72f2b07590a77932f4
Frank Horrigan <redmmond@gmail.com>
2019-09-24 23:36:11 +1000

    Request accounts every 5 sec. until logged in

--- src/app/modules/auth/epics/loadWalletsEpic.ts
+++ src/app/modules/auth/epics/loadWalletsEpic.ts
@@ -6,35 +6,40 @@
 import { Epic } from 'modules';
 import { loadWallets } from '../actions';
 import { Observable } from 'rxjs';
+import { state$ } from 'store';
 
 const loadWalletsEpic: Epic = (action$, store, { api }) => action$.ofAction(loadWallets.started)
-    .flatMap(action => {
-        const state = store.getState();
-        const network = store.getState().engine.guestSession.network;
-        const client = api({ apiHost: network.apiHost });
-
-        return Observable.from(state.storage.wallets).flatMap(wallet =>
-            Observable.from(client.keyinfo({
-                id: wallet.id
-            })).map(keyInfo => ({
-                id: wallet.id,
-                address: keyInfo.account,
-                encKey: wallet.encKey,
-                publicKey: wallet.publicKey,
-                access: keyInfo.ecosystems.map(key => ({
-                    ...key,
-                    roles: key.roles || []
+    .switchMap(action => 
+        Observable.timer(1, 5000).flatMap(() => {
+            const state = store.getState();
+            const network = store.getState().engine.guestSession.network;
+            const client = api({ apiHost: network.apiHost });
+    
+            return Observable.from(state.storage.wallets).flatMap(wallet =>
+                Observable.from(client.keyinfo({
+                    id: wallet.id
+                })).map(keyInfo => ({
+                    id: wallet.id,
+                    address: keyInfo.account,
+                    encKey: wallet.encKey,
+                    publicKey: wallet.publicKey,
+                    access: keyInfo.ecosystems.map(key => ({
+                        ...key,
+                        roles: key.roles || []
+                    }))
                 }))
-            }))
-
-        ).toArray().map(wallets => loadWallets.done({
-            params: action.payload,
-            result: wallets
-
-        })).catch(e => Observable.of(loadWallets.failed({
-            params: action.payload,
-            error: e
-        })));
-    });
+    
+            ).toArray().map(wallets => loadWallets.done({
+                params: action.payload,
+                result: wallets
+    
+            })).catch(e => Observable.of(loadWallets.failed({
+                params: action.payload,
+                error: e
+            })));
+        }).takeUntil(state$.flatMap(state => 
+            state.auth.isAuthenticated ? Observable.of(true) : Observable.empty()
+        ))
+    );
 
 export default loadWalletsEpic;
